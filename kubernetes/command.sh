7e3d08e359e87b0f56c883108f82699964c7ede8
Antoine Faul <antoine.faul@whaller.com>
2021-10-20 14:28:22 +0200

    Remove useless

--- kubernetes/command.sh
+++
@@ -1,5 +0,0 @@
-microk8s enable helm3
-microk8s kubectl create namespace cert-manager
-microk8s helm3 repo add jetstack https://charts.jetstack.io
-microk8s helm3 update
-microk8s helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --set installCRDs=true
